9839ce35cfb4e214a09dbb978b269caf824047e5
Ankit Anshu <ankit.anshu145@gmail.com>
2023-09-06 16:21:11 +0530

    FirstProject
    Covers Basics of Spring Boot and some Annotations.

---
+++ FirstProject/src/main/java/com/example/FirstProject/Laptop.java
@@ -0,0 +1,37 @@
+package com.example.FirstProject;
+
+import org.springframework.stereotype.Component;
+
+@Component("lap1")
+public class Laptop {
+    @Override
+    public String toString() {
+        return "Laptop{" +
+                "lid=" + lid +
+                ", brand='" + brand + '\'' +
+                '}';
+    }
+
+    private int lid;
+    private String brand;
+
+    public int getLid() {
+        return lid;
+    }
+
+    public void setLid(int lid) {
+        this.lid = lid;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void compile()
+    {
+        System.out.println("Compiling..");
+    }
+}
